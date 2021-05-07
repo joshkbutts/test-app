cd5749e27d1a6d42269c8834355bcd84ace7b863
joshkbutts <joshuabutts37@gmail.com>
2021-05-07 11:57:41 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉 test update hello world</Heading>
+  </Page>
+);
+
+export default Index;
